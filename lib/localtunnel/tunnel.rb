58698400f7922b2fc96b6e97d1c395ee0a4f2f21
Henry Hsu <hhsu@zendesk.com>
2012-01-25 13:12:14 -0800

    Using OpenSSH for better performance and reconnect behavior.

--- lib/localtunnel/tunnel.rb
+++ lib/localtunnel/tunnel.rb
@@ -4,6 +4,7 @@ require 'net/ssh/gateway'
 require 'net/http'
 require 'uri'
 require 'json'
+require 'open4'
 
 require 'localtunnel/net_ssh_gateway_patch'
 
@@ -45,8 +46,18 @@ class LocalTunnel::Tunnel
   def start_tunnel
     port = @port
     tunnel = @tunnel
-    gateway = Net::SSH::Gateway.new(@host, tunnel['user'], :auth_methods => %w{ publickey })
-    gateway.open_remote(port.to_i, '127.0.0.1', tunnel['through_port'].to_i) do |rp,rh|
+
+    TunnelApp.tunnel = tunnel['host']
+    tunnel_pid = fork { TunnelApp.run! }
+
+    at_exit do
+      Process.kill('KILL', tunnel_pid)
+      Process.waitpid(tunnel_pid)
+    end
+
+    error = ''
+
+    status = Open4::popen4("ssh -nNT -g -R *:#{tunnel['through_port']}:0.0.0.0:#{port} #{tunnel['user']}@#{@host} -o PasswordAuthentication=no") do |pid, i, o, e|
       puts "   " << tunnel['banner'] if tunnel.has_key? 'banner'
       if File.exists?(File.expand_path(SHELL_HOOK_FILE))
         system "#{SHELL_HOOK_FILE} ""#{tunnel['host']}""" if File.exists?(File.expand_path(SHELL_HOOK_FILE))
@@ -55,25 +66,24 @@ class LocalTunnel::Tunnel
           puts "   (Make sure it is executable)"
         end
       end
-      TunnelApp.tunnel = tunnel['host']
-      pid = fork {
-        TunnelApp.run!
-      }
       puts "   Port #{port} is now publicly accessible from http://#{tunnel['host']} ..."
+
       begin
-        sleep 1 while true
+        while str = e.gets
+          error += str
+          puts str
+        end
       rescue Interrupt
-        Process.kill('KILL', pid)
-        Process.waitpid(pid)
-        gateway.close_remote(rp, rh)
         exit
       end
     end
-  rescue Net::SSH::AuthenticationFailed
-    possible_key = Dir[File.expand_path('~/.ssh/*.pub')].first
-    puts "   Failed to authenticate. If this is your first tunnel, you need to"
-    puts "   upload a public key using the -k option. Try this:\n\n"
-    puts "   localtunnel -k #{possible_key ? possible_key : '~/path/to/key'} #{port}"
-    exit
+
+    if error =~ /Permission denied/
+      possible_key = Dir[File.expand_path('~/.ssh/*.pub')].first
+      puts "   Failed to authenticate. If this is your first tunnel, you need to"
+      puts "   upload a public key using the -k option. Try this:\n\n"
+      puts "   localtunnel -k #{possible_key ? possible_key : '~/path/to/key'} #{port}"
+      exit
+    end
   end
 end
